65ecd3cb7ae5c49629cba5aa6ea47affe7d554df
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-04-17 22:13:40 -0700

    Added functions to calculate numbers

--- js-file.js
+++ js-file.js
@@ -1,8 +1,13 @@
+let totalNumber = "";
+let useNumber;
+let inputOp;
+
+// Input numbers in text input
 function enterNumber() {
     textField = document.getElementById("numberscreen")
     buttons = document.getElementsByClassName("btn")
   
-    // Add click event listener to all button elements and insert their inner text as value to the text field
+
     Array.prototype.forEach.call (buttons, (button) => {
       button.addEventListener("click", () => {
         textField.value += button.innerText
@@ -11,6 +16,80 @@ function enterNumber() {
 }
 
 document.addEventListener("DOMContentLoaded", enterNumber);
+
+function clearScreen() {
+  document.getElementById('numberscreen').value = "";
+}
+
+function calc() {
+  if (inputOp === "+") {
+    totalNumber = parseInt(useNumber) + parseInt(document.getElementById('numberscreen').value);
+    useNumber = totalNumber;
+    alert(totalNumber);
+  } else if (inputOp === "-") {
+    totalNumber = parseInt(useNumber) - parseInt(document.getElementById('numberscreen').value);
+    useNumber = totalNumber;
+    alert(totalNumber);
+  } else if (inputOp === "x") {
+    totalNumber = parseInt(useNumber) * parseInt(document.getElementById('numberscreen').value);
+    useNumber = totalNumber;
+    alert(totalNumber);
+  } else if (inputOp === "÷") {
+    totalNumber = parseInt(useNumber) / parseInt(document.getElementById('numberscreen').value);
+    useNumber = totalNumber;
+    alert(totalNumber);
+  } else {
+
+  }
+}
+
+function addNumber() {
+  calc();
+  if (totalNumber === "") {
+    useNumber = document.getElementById('numberscreen').value;
+  }
+  if (inputOp != "+") {
+    inputOp = "+";
+  }
+  clearScreen();
+}
+
+function subtractNumber() {
+  calc();
+  if (totalNumber === "") {
+    useNumber = document.getElementById('numberscreen').value;
+  }
+  if (inputOp != "-") {
+    inputOp = "-";
+  }
+  clearScreen();
+}
+
+function multiplyNumber() {
+  calc();
+  if (totalNumber === "") {
+    useNumber = document.getElementById('numberscreen').value;
+  }
+  if (inputOp != "x") {
+    inputOp = "x";
+  }
+  clearScreen();
+}
+
+function divideNumber() {
+  calc();
+  if (totalNumber === "") {
+    useNumber = document.getElementById('numberscreen').value;
+  }
+  if (inputOp != "÷") {
+    inputOp = "÷";
+  }
+  clearScreen();
+}
+
+function equalNumber() {
+  calc();
+}
 
 
 
